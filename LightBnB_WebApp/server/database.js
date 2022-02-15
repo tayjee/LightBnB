e2527562d5948878960a1bd370e72cab3240a604
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-02-15 02:56:21 +0000

    lighthouse bnb reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -60,10 +60,11 @@ exports.getUserWithId = getUserWithId;
       `
       INSERT INTO users (name, email, password)
       VALUES ($1, $2, $3)
-      RETURNING *;`,
+      RETURNING *;
+      `,
       [ user.name, user.email, user.password ]
       )
-      .then((result) => result.rows[0] || null)
+      .then((user) => user.rows[0] || null)
       .catch((err) => {
         console.log(err.message);
       });
@@ -79,8 +80,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool
+    .query(
+    `SELECT * FROM reservations
+    JOIN properties ON properties.id = property_id
+    WHERE guest_id = $1
+    LIMIT $2 `,
+      [guest_id, limit]
+    )
+    .then((result) => result.rows || null)
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
